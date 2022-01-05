dcd2399d946632029e3c6ab9946ed10bb196180b
kilianmarcell <kilian.marcell.andras@gmail.com>
2022-01-05 13:11:31 +0100

    Alapvető adatok hozzáadva

--- speedruns/database/seeders/DatabaseSeeder.php
+++ speedruns/database/seeders/DatabaseSeeder.php
@@ -13,12 +13,33 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //\App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
         //\App\Models\Game::factory(5)->create();
-        \App\Models\Game::factory(5)->create(
+        \App\Models\Game::factory(2)->create(
             ['name' => 'Mario 2']);
 
-        \App\Models\Game::factory(5)->create(
+        \App\Models\Game::factory()->create(
             ['name' => 'Portal 2']);
+
+            \App\Models\Records::factory()->create([
+                'user_id' => 1,
+                'game_id' => 1,
+                'category' => 'any%',
+                'time' => 155
+            ]);
+
+            \App\Models\Records::factory()->create([
+                'user_id' => 2,
+                'game_id' => 2,
+                'category' => 'glitch-free',
+                'time' => 213
+            ]);
+
+            \App\Models\Records::factory()->create([
+                'user_id' => 3,
+                'game_id' => 3,
+                'category' => 'any%',
+                'time' => 421
+            ]);
     }
 }
